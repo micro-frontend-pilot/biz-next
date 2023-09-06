f392f7d7d9a84dd122921c8db6b1d8c5e64c7faf
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2023-09-06 19:31:45 +0900

    Create UserList.jsx

---
+++ pages/components/UserList.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+const UserList = (props) => {
+
+    console.log("User list는 웹 브라우저에서 렌더링 됩니다.")
+    return (
+        <div>
+            User List는 CSR 방식의 마이크로 프론트엔드
+        </div>
+    )
+}
+
+export default UserList;
